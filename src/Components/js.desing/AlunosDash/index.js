c8fe9d0b3089776c1aa7f95e001d842f2bfa6e41
Joao <joaovictorgacunha@gmail.com>
2024-05-14 17:14:04 -0300

    versao com novas pastas 2.0

--- src/Components/js.desing/AlunosDash/index.js
+++ src/Components/js.desing/AlunosDash/index.js
@@ -2,8 +2,7 @@ import CardsGeral from "../ProfessorDash"
 import Editar from '../ModelPostarAula/index'
 export default function Alunos(){
     return(
-        <CardsGeral txt1="joao para de ser burro">
-        </CardsGeral>
+        
 
     )
 }
